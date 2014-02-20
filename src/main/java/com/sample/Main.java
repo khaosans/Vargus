d2f4599c851230d4b44f97e16768655fdbd7aa6e
Souriya Khaosanga <souriya.khaosanga@gmail.com>
2014-02-19 16:34:25 -0800

    initial commit

---
+++ src/main/java/com/sample/Main.java
@@ -0,0 +1,17 @@
+package com.sample;
+
+import static org.apache.commons.lang.RandomStringUtils.randomAlphanumeric;
+
+/**
+ * Created by souriyakhaosanga on 2/5/14.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("dfas");
+
+        String a = randomAlphanumeric(5);
+
+        System.out.println(a);
+
+    }
+}
---
+++ src/test/java/com/sample/MainTest.java
@@ -0,0 +1,17 @@
+package com.sample;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static org.apache.commons.lang.RandomStringUtils.randomAlphabetic;
+import static org.apache.commons.lang.RandomStringUtils.randomNumeric;
+
+/**
+ * Created by souriyakhaosanga on 2/5/14.
+ */
+public class MainTest {
+    @Test
+    public void testTest() throws Exception{
+
+    }
+}
