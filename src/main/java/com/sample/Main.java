ba16a3b011484a1bf7d22308c0907ae3aec5df1d
Souriya Khaosanga <souriya.khaosanga@gmail.com>
2014-02-20 00:28:54 -0800

    create a set of numbers used to test the avl

--- src/main/java/com/sample/Main.java
+++ src/main/java/com/sample/Main.java
@@ -8,32 +8,5 @@ import static org.apache.commons.lang.RandomStringUtils.randomNumeric;
  */
 public class Main {
     public static void main(String[] args) {
-
-
-//        String a = randomNumeric(1);
-//        Integer.parseInt(a);
-//        System.out.println(a);
-//        System.out.println("hello");
-        AVLTree tree = new AVLTree();
-        tree.insert(Integer.parseInt(randomNumeric(2)));
-        tree.insert(Integer.parseInt(randomNumeric(2)));
-        tree.insert(Integer.parseInt(randomNumeric(2)));
-        tree.insert(Integer.parseInt(randomNumeric(2)));
-        tree.insert(Integer.parseInt(randomNumeric(2)));
-        tree.insert(Integer.parseInt(randomNumeric(2)));
-        tree.insert(Integer.parseInt(randomNumeric(2)));
-        tree.insert(Integer.parseInt(randomNumeric(2)));
-        tree.insert(Integer.parseInt(randomNumeric(2)));
-        tree.insert(Integer.parseInt(randomNumeric(2)));
-        tree.insert(Integer.parseInt(randomNumeric(2)));
-        tree.insert(Integer.parseInt(randomNumeric(2)));
-        tree.insert(Integer.parseInt(randomNumeric(2)));
-        tree.insert(Integer.parseInt(randomNumeric(2)));
-        tree.insert(Integer.parseInt(randomNumeric(2)));
-        tree.insert(Integer.parseInt(randomNumeric(2)));
-        tree.insert(Integer.parseInt(randomNumeric(2)));
-        tree.display();
-
-
     }
 }
--- src/test/java/com/sample/MainTest.java
+++ src/test/java/com/sample/MainTest.java
@@ -2,16 +2,55 @@ package com.sample;
 
 import org.junit.Test;
 
-import static junit.framework.Assert.assertEquals;
-import static org.apache.commons.lang.RandomStringUtils.randomAlphabetic;
+import java.util.Random;
+import java.util.Stack;
+import java.util.UUID;
+
 import static org.apache.commons.lang.RandomStringUtils.randomNumeric;
 
 /**
  * Created by souriyakhaosanga on 2/5/14.
  */
-public class MainTest {
+public class MainTest extends AVLTree {
+    private static int [] array;
+    private static int maxValue;
     @Test
     public void testTest() throws Exception{
 
+        buildArray();
+        randomizeArray(array);
+
+        Stack<Integer> stack = new Stack();
+        Stack<Integer> copy = new Stack();
+
+        for(int i = 0; i<20000;++i){
+            AVLTree tree = new AVLTree();
+            stack.push(array[i]);
+            copy.push(array[i]);
+            tree.insert(stack.pop());
+        }
+
+
+    }
+
+    public static int [] buildArray(){
+        array = new int[maxValue];
+        for(int i = 0; i< maxValue;++i){
+            array[i]=i;
+        }
+        return array;
+    }
+
+    public static int[] randomizeArray(int[] array){
+        Random rgen = new Random();  // Random number generator
+
+        for (int i=0; i<array.length; i++) {
+            int randomPosition = rgen.nextInt(array.length);
+            int temp = array[i];
+            array[i] = array[randomPosition];
+            array[randomPosition] = temp;
+        }
+
+        return array;
     }
 }
