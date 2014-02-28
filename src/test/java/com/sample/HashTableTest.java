ed5a07213cd177afb7439b0a4df79dea0eed3b96
Souriya Khaosanga <souriya.khaosanga@gmail.com>
2014-02-27 21:08:40 -0800

    added a test for balanced tree and valid bst.  Added multiple test functions int AVLTree

--- src/test/java/com/sample/HashTableTest.java
+++ src/test/java/com/sample/HashTableTest.java
@@ -7,13 +7,11 @@ import org.junit.Test;
  * Created by locle on 2/26/14.
  */
 public class HashTableTest extends TestCase {
+    private int max=50;
     @Test
     public void testInsert() throws Exception {
         HashTable h = new HashTable();
-
-
     }
-
     @Test
     public void testRemove() throws Exception {
 
