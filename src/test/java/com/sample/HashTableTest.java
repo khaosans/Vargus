0ac287082be02ef9a8fb7553a326e8132e70b8de
Souriya Khaosanga <souriya.khaosanga@gmail.com>
2014-02-28 18:34:03 -0800

    creae a hashtabletest.java file

---
+++ src/test/java/com/sample/HashTableTest.java
@@ -0,0 +1,29 @@
+package com.sample;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * Created by souriyakhaosanga on 2/27/14.
+ */
+public class HashTableTest extends TestCase {
+    @Test
+    public void testInsert() throws Exception {
+
+    }
+
+    @Test
+    public void testDisplay() throws Exception {
+
+    }
+
+    @Test
+    public void testFind() throws Exception {
+
+    }
+
+    @Test
+    public void testRemove() throws Exception {
+
+    }
+}
